fcb5054585f0ab5d67005e7147db11c946775930
felipespinassi <felipe_spinassi@hotmail.com>
2022-03-26 15:13:11 -0300

    styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { GlobalStyle } from "./styles/global";
 
 function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
+      <GlobalStyle/>
     </div>
   );
 }
